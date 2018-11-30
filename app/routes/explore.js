787d43ffe3a17ba6deb7f1f7c6daa8f6406f8339
William <yuanx@usc.edu>
2018-11-29 20:40:52 -0800

    landing page complete

---
+++ app/routes/explore.js
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+import 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCHRcQG1xJXjbBj7tUEc6LaEzSDrtnb894&libraries=places';
+
+export default Route.extend({
+	
+	
+});
